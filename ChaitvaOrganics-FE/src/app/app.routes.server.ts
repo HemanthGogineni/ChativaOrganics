017f80044079769c217373b1c965b19ddd8979ba
Hemanth Gogineni <hemanthgogineni485@gmail.com>
2025-09-02 09:23:16 +0530

    Fix prod build

--- ChaitvaOrganics-FE/src/app/app.routes.server.ts
+++ ChaitvaOrganics-FE/src/app/app.routes.server.ts
@@ -2,6 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'product/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const ids = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
+        '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26'];
+      return ids.map(id => ({ id }));
+    },
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
